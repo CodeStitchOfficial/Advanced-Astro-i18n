ba446014d82a55bf8156df35725bda90d54d0e59
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2026-03-13 21:01:07 +0800

    reworking BaseLayout and Meta for i18n

--- src/config/routeTranslations.ts
+++ src/config/routeTranslations.ts
@@ -6,12 +6,14 @@ export const routeTranslations: Record<Locale, Record<string, string>> = {
     "projects": "projects",
     "project-1": "project-1",
     "project-2": "project-2",
+    "reviews": "reviews",
   },
   fr: {
     "about": "a-propos",
     "projects": "projets",
     "project-1": "projet-1",
     "project-2": "projet-2",
+    "reviews": "avis",
   },
 };
 
