225538a68a5d0d7cb5214538d44c470213874ea8
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2026-03-19 16:12:25 +0100

    cleaning up utils

--- src/js/utils.ts
+++ src/js/utils.ts
@@ -10,11 +10,3 @@ export function formatDate(date: string | number | Date): string {
 export function getCurrentYear(): number {
   return new Date().getFullYear();
 }
-
-export function trimArrSlashes(arr: string[]) {
-  return arr.map((str) => str.replace(/^\/+|\/+$/g, ""));
-}
-
-export function trimStringSlashes(arr: string) {
-  return arr.replace(/^\/+|\/+$/g, "");
-}
