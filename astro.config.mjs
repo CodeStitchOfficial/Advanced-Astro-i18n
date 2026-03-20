351bcaa86246d3b31337b9550a56d1fcf983e7c8
Ayres Vitor <gitkey@virtuaires.com.br>
2026-03-20 15:20:32 -0300

    inital migration to fonts api
    move fonts to src

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
@@ -24,5 +24,55 @@ export default defineConfig({
 				},
 			},
 		}),
+	],
+	fonts: [
+		// Option 1: Fontsource (uncomment this and comment out Option 1 to use)
+		// You must visit https://fontsource.org/fonts/roboto/install
+		// and run the specific install command - no need to change anything else
+		// {
+		// 	provider: fontProviders.fontsource(),
+		// 	name: "Roboto",
+		// 	cssVariable: "--font-primary",
+		// 	fallbacks: ["Arial", "sans-serif"],
+		// 	weights: [400, 700, 900],
+		// 	styles: ["normal"],
+		// },
+		// Option 2: local files /src/assets/fonts (uncomment this and comment out Option 1 to use)
+		{
+			provider: fontProviders.local(),
+			name: "Roboto",
+			cssVariable: "--font-primary",
+			fallbacks: ["Arial", "sans-serif"],
+			formats: ["woff2", "woff"],
+			options: {
+				variants: [
+					{
+						src: [
+							"./src/assets/fonts/roboto-v29-latin-regular.woff2",
+							"./src/assets/fonts/roboto-v29-latin-regular.woff",
+						],
+						weight: 400,
+						style: "normal",
+					},
+					{
+						src: [
+							"./src/assets/fonts/roboto-v29-latin-700.woff2",
+							"./src/assets/fonts/roboto-v29-latin-700.woff",
+						],
+						weight: 700,
+						style: "normal",
+					},
+					{
+						src: [
+							"./src/assets/fonts/roboto-v29-latin-900.woff2",
+							"./src/assets/fonts/roboto-v29-latin-900.woff",
+						],
+						weight: 900,
+						style: "normal",
+					},
+				],
+			},
+		},
+
 	],
 });
